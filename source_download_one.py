fc0aa62956c2b4e0d909aba0ba58afa5897b09ab
kakmond <wongsathorn.pa@ku.th>
2019-07-01 22:39:49 +0900

    [Updated]: write result to file

--- source_download_one.py
+++ source_download_one.py
@@ -15,14 +15,14 @@ logs = subprocess.run(cmd, stdout=subprocess.PIPE)
 
 os.chdir(path)
 
-graph = graph.Graph.getInstance() # call graph singleton instance
+graph = graph.Graph() # initialize the graph object
 
 cmd = 'sudo apt-get source ' + pkgName
 hash_cmd = hash_lib.sha256string(cmd)
 graph.add_vertex(hash_cmd)
 logs = subprocess.call(cmd, shell=True)
 
-# log the output of 'apt-get source' command to graph instance
+# log the output of 'apt-get source' command to graph object
 for root, dirs, files in os.walk(path): 
    for filename in files:
         checksum = hash_lib.sha256sum(filename)
@@ -33,7 +33,7 @@ hash_cmd = hash_lib.sha256string(cmd)
 graph.add_vertex(hash_cmd)
 logs = subprocess.call(cmd, shell=True)
 
-# log the output of 'apt-get build-dep' command to graph instance
+# log the output of 'apt-get build-dep' command to graph object
 logs = subprocess.check_output('apt-cache showsrc ' + pkgName + ' | grep -oP "(?<=Build-Depends:).*"', universal_newlines=True, shell=True) # list packages needed to build from source
 deps = logs.split(',')
 for dep in deps:
@@ -45,11 +45,16 @@ for dep in deps:
                if isFile:
                         checksum = hash_lib.sha256sum(path)
                         graph.add_edge(hash_cmd, checksum, path) # add edge connecting 'apt-get build-dep' command to output file
-                        
-# for v in graph:
-# 	for w in v.get_connections():
-# 		vid = v.get_id()
-# 		wid = w.get_id()
-# 		print (vid, wid)
+
+# create /buildtrace/{packageName}/graph folder
+working_dir = './temp/' + pkgName + '/'
+for x in os.listdir(working_dir):
+    if os.path.isdir(working_dir + x):
+        cmd = ['mkdir', '-p', '/buildTrace/' + x + '/graph'] 
+        logs = subprocess.run(cmd, stdout=subprocess.PIPE)
+        # write the graph data structure to file
+        for node in graph:
+            with open('/buildTrace/' + x + '/graph/graph_all.txt', 'a') as graph_file:
+                    graph_file.write(node.get_id() + ' : ' + graph.vert_dict[node.get_id()] + '\n')
 
 os.chdir('../../')
