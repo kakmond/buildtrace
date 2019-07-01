b7dace5df15a8d5b71d6c0ea609fc5d5845ae6d5
kakmond <wongsathorn.pa@ku.th>
2019-07-01 16:06:31 +0900

    [Updated]: log input and output of command to graph instance

--- strace.py
+++ strace.py
@@ -3,12 +3,18 @@ import subprocess
 import glob
 import os
 import re
-import hashlib
 import time
+import graph
+
+import hash_lib
 
 # subprocess内でルートを指定したいときの書き方
 # os.path.expanduser('~/')
 pkgName = ''
+buildCmd = ''
+
+# call graph singleton instance
+graph = graph.Graph.getInstance()
 
 # 最初の処理で/buildTrace/pkgName内を全削除するため確認を取るための関数
 def yes_no_input():
@@ -20,12 +26,13 @@ def yes_no_input():
             return False
 
 # main関数
-def straceExe(buildCmd, pkgName_):
-    global pkgName
+def straceExe(buildCmd_, pkgName_):
+    global pkgName, buildCmd
     pkgName = pkgName_
+    buildCmd = buildCmd_
     removeDir()
     makeDir()
-    strace(buildCmd)
+    strace()
     log_edit()
     file_exist()
     backup()
@@ -76,12 +83,15 @@ def makeDir():
     # print(logs.stdout.decode())
 
 @countTime
-def strace(buildCmd):
+def strace():
     # ビルドコマンド入力，加工
     buildcmd = buildCmd
     if buildcmd == '':
         buildcmd = input("Please Enter build command : ")
     cmd = 'strace -ff -e trace=openat,open -o /buildTrace/' + pkgName + '/logs/strace_out.txt ' + buildcmd
+
+    hash_cmd = hash_lib.sha256string(buildcmd)     # encryte build command to hash
+    graph.add_vertex(hash_cmd) # add hash to graph
 
     # straceコマンドを使用 /buildTrace/にout.txt.????として出力(????はPID)
     logs = subprocess.run(cmd, stdout=subprocess.PIPE, shell=True)
@@ -220,9 +230,10 @@ def hash_output():
     logfile = open('/buildTrace/' + pkgName + '/backup/hash/input_hash_list.txt', 'wt')
     file = open('/buildTrace/' + pkgName + '/backup/hash/input_hash_only.txt', 'wt')
     for i in lines:
-        checksum = sha256sum(i)
+        checksum = hash_lib.sha256sum(i)
         logfile.write(checksum + ' : ' + i + '\n')
         file.write(checksum + '\n')
+        graph.add_edge(checksum, buildCmd, i) # add edge connecting input file to build command
     logfile.close()
     file.close()
 
@@ -232,9 +243,10 @@ def hash_output():
     logfile = open('/buildTrace/' + pkgName + '/backup/hash/output_hash_list.txt', 'wt')
     file = open('/buildTrace/' + pkgName + '/backup/hash/output_hash_only.txt', 'wt')
     for i in lines:
-        checksum = sha256sum(i)
+        checksum = hash_lib.sha256sum(i)
         logfile.write(checksum + ' : ' + i + '\n')
         file.write(checksum + '\n')
+        graph.add_edge(buildCmd, checksum, i) # add edge connecting build command to output file
     logfile.close()
     file.close()
 
@@ -257,16 +269,16 @@ def hash_output():
     # sha256 = hashlib.sha256()
     # with open('/buildTrace/' + pkgName + '/backup/hash/input_hash_only.txt', 'rb') as f:
         # inputHash = hashlib.sha256(f.read().encode()).hexdigest()
-    inputHash = sha256sum('/buildTrace/' + pkgName + '/backup/hash/input_hash_only.txt')
+    inputHash = hash_lib.sha256sum('/buildTrace/' + pkgName + '/backup/hash/input_hash_only.txt')
 
     # sha256 = hashlib.sha256()
     # with open('/buildTrace/' + pkgName + '/backup/hash/output_hash_only.txt', 'rb') as f:
         # outputHash = hashlib.sha256(f.read().encode()).hexdigest()
-    outputHash = sha256sum('/buildTrace/' + pkgName + '/backup/hash/output_hash_only.txt')
+    outputHash = hash_lib.sha256sum('/buildTrace/' + pkgName + '/backup/hash/output_hash_only.txt')
 
     # with open('/buildTrace/' + pkgName + '/backup/command/buildCommand.txt', 'rb') as f:
         # commandHash = hashlib.sha256(f.read().encode()).hexdigest()
-    commandHash = sha256sum('/buildTrace/' + pkgName + '/backup/command/buildCommand.txt')
+    commandHash = hash_lib.sha256sum('/buildTrace/' + pkgName + '/backup/command/buildCommand.txt')
 
     with open('/buildTrace/' + pkgName + '/backup/hash/hash_all.txt', 'wt') as f:
         f.write('inputHash   : ' + inputHash + '\n')
@@ -274,12 +286,12 @@ def hash_output():
         f.write('commandHash : ' + commandHash + '\n')
 
 #ハッシュ計算
-def sha256sum(filename):
-    h = hashlib.sha256()
-    with open(filename, 'rb', buffering=0) as f:
-        for b in iter(lambda : f.read(128*1024), b''):
-            h.update(b)
-    return h.hexdigest()
+# def sha256sum(filename):
+#     h = hashlib.sha256()
+#     with open(filename, 'rb', buffering=0) as f:
+#         for b in iter(lambda : f.read(128*1024), b''):
+#             h.update(b)
+#     return h.hexdigest()
 
 def exeTime_edit(funclist):
     exeTime_edit_List = []
