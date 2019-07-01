3e36ac3d07a31b967de275992221814d2ef85fef
kakmond <wongsathorn.pa@ku.th>
2019-07-01 23:07:44 +0900

    [Fixed]: correct wrong name variable

--- source_download_one.py
+++ source_download_one.py
@@ -48,11 +48,14 @@ for dep in deps:
 os.chdir('../../')
 
 # create /buildtrace/{packageName}/graph folder
-for x in os.listdir(path):
-    if os.path.isdir(path + x):
+working_dir = './temp/' + pkgName + '/'
+for x in os.listdir(working_dir):
+    if os.path.isdir(working_dir + x):
         cmd = ['mkdir', '-p', '/buildTrace/' + x + '/graph'] 
         logs = subprocess.run(cmd, stdout=subprocess.PIPE)
         # write the graph data structure to file
         for node in graph:
-            with open('/buildTrace/' + x + '/graph/graph_all.txt', 'a') as graph_file:
-                    graph_file.write(node.get_id() + ' : ' + graph.vert_dict[node.get_id()] + '\n')
+                for edge in node.get_connections():
+                    with open('/buildTrace/' + x + '/graph/graph_all.txt', 'a') as graph_file:
+                        graph_file.write(node.get_id() + ' : ' + edge.get_id() + '\n')
+        break
